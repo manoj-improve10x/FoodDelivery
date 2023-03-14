960ce150fe5ba3cfc0163d28bc4d7595f12f26a3
manoj <manoj.mopoori@improve10x.com>
2023-03-14 15:06:59 +0530

    Implemented SplashScreenActivity

---
+++ app/src/main/java/com/example/foodapplication/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.foodapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, FoodActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
